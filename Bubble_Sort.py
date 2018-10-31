2aff0efb6afc75eacbf584840cbc2719f548e67b
jub2726 <jub2726@gmail.com>
2018-10-31 15:40:42 +0900

    버블정렬 완성

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,6 +1,13 @@
 
 arr = []
-
-for i in range(10):
+arr_len = 10
+for i in range(arr_len):
     arr.append(int(input()))
 
+for i in range(0, arr_len - 1):
+    for j in range(0, arr_len - i - 1):
+        if arr[j] > arr[j+1]:
+            temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+print(arr)
